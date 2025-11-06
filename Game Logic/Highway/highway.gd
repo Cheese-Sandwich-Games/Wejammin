96d5f1abe8037a9af6dbad2788d37f95775b2caa
marcbosmancode <61054760+marcbosmancode@users.noreply.github.com>
2025-11-06 21:31:22 +0100

    Add note spawning

--- Game Logic/Highway/highway.gd
+++ Game Logic/Highway/highway.gd
@@ -5,6 +5,10 @@ class_name Highway
 @export var spawn_data: CurveTexture
 
 
-func spawn_note(_lane_number: int) -> void:
-	# TODO add notes to the given lane. Can use child index to find given lane
-	pass
+func spawn_note(lane_number: int) -> void:
+	# Get the given lane and add a note to it
+	var lane: Node = get_child(lane_number - 1)
+	
+	# Verify the lane number is valid before trying to spawn the note
+	if lane is Lane:
+		lane.spawn_note()
--- Game Logic/Highway/lane.gd
+++ Game Logic/Highway/lane.gd
@@ -2,11 +2,14 @@ extends Node2D
 class_name Lane
 
 
+var music_note = preload("res://Game Logic/Highway/music_note.tscn")
+
 @onready var current_notes = $Notes
 @onready var note_origin = $NoteOrigin
 @onready var note_destination = $NoteDestination
 
 
 func spawn_note() -> void:
-	# TODO add notes to the lane
-	pass
+	# Add a note to this lane
+	var note_instance = music_note.instantiate()
+	current_notes.add_child(note_instance)
