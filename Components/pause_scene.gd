060335594c4e80f03882e9ba25ce3dbc8ae2a2f7
Cheese-Sandwich-Games <adrian12@berger-mail.net>
2025-11-08 19:06:50 +0100

    Added Functionality for quitting the game

--- Components/pause_scene.gd
+++ Components/pause_scene.gd
@@ -3,7 +3,8 @@ extends Node2D
 @export var pause_menu :PauseMenu
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if pause_menu:
+		pause_menu.quit_game_pressed.connect(_on_superquit_pressed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,14 +12,5 @@ func _process(delta):
 	pass
 
 
-func _on_pause_component_game_paused():
-	pause_menu.show()
-	Globals.is_game_running = false
-
-func _on_pause_component_game_unpaused():
-	pause_menu.hide()
-	Globals.is_game_running = true
-
-func _on_pause_menu_continue_pressed():
-	pause_menu.hide()
-	Globals.is_game_running = true
+func _on_superquit_pressed():
+	get_tree().quit()
