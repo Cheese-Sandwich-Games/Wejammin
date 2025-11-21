5a2cb9a997e9da27056ef44bec9ff35f8e05b606
Cheese-Sandwich-Games <adrian12@berger-mail.net>
2025-11-21 13:32:19 +0100

    added more particles

--- Game Logic/Highway/music_note.gd
+++ Game Logic/Highway/music_note.gd
@@ -5,10 +5,14 @@ class_name MusicNote
 const MOVE_BUFFER: float = 0.06
 
 var move_tween: Tween
+var is_hitted: bool = false
 
 @onready var life_timer = $LifeTimer
 
+@export var perfect_particle_emitter: GPUParticles2D
+#@export var good_particle_emitter: GPUParticles2D
 
+@export var sprite_ref: Sprite2D
 func initialize(destination_point: Vector2, move_duration: float) -> void:
 	# Add a little buffer at the end where the note keeps going
 	var move_duration_with_buffer = move_duration * (1 + MOVE_BUFFER)
@@ -26,20 +30,30 @@ func handle(is_hit: bool, is_perfect: bool = false) -> void:
 	if is_hit:
 		Globals.combo += 1
 		Globals.notes_hit += 1
-		if is_perfect:
+		if is_perfect && !is_hitted:
+			is_hitted = true
 			Globals.perfect_hits += 1
 			print("Note perfect hit")
-		else:
+			perfect_particle_emitter.emitting = true
+			sprite_ref.hide()
+			await perfect_particle_emitter.finished
+		elif !is_hitted:
+			is_hitted = true
 			Globals.good_hits += 1
 			print("Note good hit")
+			#good_particle_emitter.emitting = true
+			#sprite_ref.hide()
+			#await good_particle_emitter.finished
 	else:
 		Globals.combo = 0
 		Globals.notes_missed += 1
 		print("Note missed")
-	
+		
+
 	print("Combo: ", Globals.combo, " Notes hit: ", Globals.notes_hit, " Notes missed: ", Globals.notes_missed, " Perfect notes: ", Globals.perfect_hits, " Good notes: ", Globals.good_hits)
 	
 	# Delete a note that has been handled
+	
 	queue_free()
 
 
